c57fde2b51f973eea577c1534b2ecc163a68f971
Gonzalo Albornoz <gonzalo.hr.albornoz@gmail.com>
2024-08-18 13:54:47 -0300

    agrego model Cuenta

---
+++ Services/AuthService/Models/Cuenta.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
