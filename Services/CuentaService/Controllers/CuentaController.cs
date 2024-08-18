f3733617bbc48b2c8f9600f94d63c78f437cd4dd
Gonzalo Albornoz <gonzalo.hr.albornoz@gmail.com>
2024-08-18 19:48:53 -0300

    metodo de obtencion de saldo

--- Services/CuentaService/Controllers/CuentaController.cs
+++ Services/CuentaService/Controllers/CuentaController.cs
@@ -1,4 +1,10 @@
+using AuthService.Repositories;
+using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace CuentaService.Controllers
 {
@@ -6,11 +12,47 @@ namespace CuentaService.Controllers
     [Route("")]
     public class CuentaController : ControllerBase
     {
+        private ILogger _logger;
+        private ICuentaRepository _cuentaRepository;
 
+        public CuentaController(ILogger<CuentaController> logger, ICuentaRepository cuentaRepository)
+        {
+            _logger = logger;
+            _cuentaRepository = cuentaRepository;
+        }
         [HttpGet("health")]
         public IActionResult Health()
         {
             return Ok();
         }
+
+        [HttpGet("saldo"), Authorize]
+        public async Task<IActionResult> Saldo()
+        {
+            var cuentaId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(cuentaId == null)
+            {
+                BadRequest();
+            }
+
+            var cuenta = await _cuentaRepository.GetAsync(int.Parse(cuentaId!));
+
+            if (cuenta == null)
+            {
+                BadRequest();
+            }
+
+            return Ok(new { saldo = cuenta!.Saldo });
+
+        }
+
+        //[HttpPost("cashin")]
+        //public async Task<IActionResult> CashIn() {
+        
+        
+        //}
+
+
     }
 }
