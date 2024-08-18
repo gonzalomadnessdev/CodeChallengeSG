74932a3cc624ce246afb6820e49917f31ada2fe6
Gonzalo Albornoz <gonzalo.hr.albornoz@gmail.com>
2024-08-18 20:29:05 -0300

    metodo crear movimiento

---
+++ Services/CuentaService/Repositories/CuentaMovimientoDBRepository.cs
@@ -0,0 +1,25 @@
+﻿using AuthService.Repositories;
+using CuentaService.Models;
+using Dapper;
+
+namespace CuentaService.Repositories
+{
+    public class CuentaMovimientoDBRepository : BaseDBRepository
+    {
+        public CuentaMovimientoDBRepository(DapperContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> Crear(CuentaMovimiento movimiento)
+        {
+            const string sql = "INSERT INTO CuentaMovimiento (CuentaId, Importe, Saldo) VALUES (@cuentaId, @importe, @saldo)";
+
+            using (var conn = _context.CreateConnection())
+            {
+                var result = await conn.ExecuteAsync(sql, new { cuentaId = movimiento.CuentaId, importe = movimiento.Importe, saldo = movimiento.Saldo });
+                return result > 0;
+            }
+        }
+
+    }
+}
